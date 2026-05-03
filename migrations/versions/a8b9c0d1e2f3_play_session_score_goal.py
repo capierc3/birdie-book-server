f56ea9f081cbfc5b129959a758c9042d4231bc1e
Chase Pierce <capierc3@asu.edu>
2026-05-02 22:47:06 -0400

    Update a8b9c0d1e2f3_play_session_score_goal.py

--- migrations/versions/a8b9c0d1e2f3_play_session_score_goal.py
+++ migrations/versions/a8b9c0d1e2f3_play_session_score_goal.py
@@ -14,7 +14,7 @@ from alembic import op
 
 
 revision: str = 'a8b9c0d1e2f3'
-down_revision: Union[str, None] = 'f6a7b8c9d0e1'
+down_revision: Union[str, None] = 'a7b8c9d0e1f2'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
